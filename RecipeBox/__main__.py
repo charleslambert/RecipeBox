1c39ad78e7b29b3383df5b2ac0144a28ee773183
Charles Lee Lambert <c.lambert1213@gmail.com>
2018-10-25 09:55:39 -0700

    Update main

--- RecipeBox/__main__.py
+++ RecipeBox/__main__.py
@@ -1,4 +1,85 @@
-from prompts import list_items
+import textwrap
+from fractions import Fraction
 
-sel = list_items("Its a world", ["hello", "world"])
-print(sel)
+import click
+
+from prompts import amount_prompt, list_items, sanitized_input, y_n_prompt
+from recipe import Recipe
+from store import Store
+
+
+def to_tuple(fraction):
+    return (fraction.numerator, fraction.denominator)
+
+
+def new_recipe():
+    recipe = {"title": "", "ingredients": [], "instructions": []}
+
+    recipe["title"] = input("Recipe Name:\n  ")
+
+    while True:
+        print("Add an ingredient:")
+        ingredient = {}
+
+        ingredient["name"] = sanitized_input("  Name: ", str)
+        ingredient["amount"] = to_tuple(amount_prompt("  Amount: "))
+        ingredient["measure"] = sanitized_input("  Measurment: ", str)
+        recipe["ingredients"].append(ingredient)
+
+        if y_n_prompt("Add more ingredients", "n") == "n":
+            break
+    while True:
+        print("Add an instruction:")
+        recipe["instructions"].append(sanitized_input("  Instruction: ", str))
+
+        if y_n_prompt("Add more instructions", "n") == "n":
+            break
+
+    return recipe
+
+
+def print_recipe(recipe):
+    print("\n{}\n".format(recipe["title"]))
+    for ingredient in recipe["ingredients"]:
+        num, den = ingredient["amount"]
+        if (num / den) % 1 == 0 or (num / den) < 1:
+            formated_amount = "{}".format(str(Fraction(num, den)))
+        else:
+            formated_amount = "{} {}".format(
+                int(num / den), str(Fraction(int(num / den), den)))
+
+        print("  {:>5} {:<10}{:<}".format(
+            formated_amount, ingredient["measure"], ingredient["name"]))
+
+    print()
+    for i, instruction in enumerate(recipe["instructions"]):
+        wrapped_lines = textwrap.wrap(instruction)
+        print(" {:>2}: {}".format(i + 1, wrapped_lines[0]))
+        for line in wrapped_lines[1:]:
+            print("     {}".format(line))
+        print()
+
+
+@click.command()
+def main():
+    store = Store()
+    recipes = store.load()
+    recipes = [Recipe.from_dict(x) for x in recipes]
+    while True:
+        selection = list_items("Recipe Box",
+                               ["Add a recipe", "Read a recipe", "Quit"])
+
+        if selection == 0:
+            recipes.append(new_recipe())
+            store.save(recipes)
+
+        if selection == 1:
+            r = list_items("Recipes", [recipe.title for recipe in recipes])
+            print(recipes[r])
+
+        if selection == 2:
+            break
+
+
+if __name__ == '__main__':
+    main()
--- RecipeBox/prompts.py
+++ RecipeBox/prompts.py
@@ -1,3 +1,6 @@
+from fractions import Fraction
+
+
 def _find_getch():
     try:
         import termios
@@ -7,7 +10,8 @@ def _find_getch():
         return msvcrt.getch
 
     # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
+    import sys
+    import tty
 
     def _getch():
         fd = sys.stdin.fileno()
@@ -25,13 +29,53 @@ def _find_getch():
 getch = _find_getch()
 
 
+def y_n_prompt(prompt, default):
+    if default == "y":
+        y_n = "[Y/n]"
+    else:
+        y_n = "[y/N]"
+
+    print(prompt, y_n)
+    while True:
+        char = getch()
+
+        if char == "\r":
+            return default
+        if char in ["y", "Y", "n", "N"]:
+            return char.lower()
+
+
 def list_items(prompt, items):
-    selection = -1
 
     print(prompt)
-    while selection not in range(len(items)):
-        for i, item in enumerate(items):
-            print(f"  {i}: {item}")
-        selection = int(getch())
+    for i, item in enumerate(items):
+        print(f"  {i}: {item}")
 
-    return items[selection]
+    while True:
+        try:
+            selection = int(getch())
+            if selection in range(len(items)):
+                return selection
+        except ValueError:
+            pass
+
+
+def sanitized_input(prompt, typeof):
+    while True:
+        try:
+            inp = typeof(input(prompt))
+            return inp
+        except ValueError:
+            print(f"Should be {typeof}")
+
+
+def amount_prompt(prompt):
+    while True:
+        try:
+            inp = input(prompt).split(" ")
+            if len(inp) < 2:
+                return Fraction(inp[0])
+
+            return Fraction(inp[0]) + Fraction(inp[1])
+        except (ValueError, NameError):
+            print(f"Invalid amout can be whole, fraction or mixed number")
